fda9f366a84166ff1795ebdb7cea4104901dea80
Yaroslav Boyko <Spectraldagger322@gmail.com>
2021-08-22 20:14:11 +0300

    All constraints done well

--- ColorizedLabel/ViewController.swift
+++ ColorizedLabel/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var redColorValue: UILabel!
+    @IBOutlet var redColorSlider: UISlider!
+    
+//    let mainViewColors = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func redSliderAction() {
+        redColorValue.text = String(format: "%.2f", redColorSlider.value)
+        let colorValue = CGFloat(redColorSlider.value)
+        mainView.alpha = colorValue
+    }
+    
 }
 
